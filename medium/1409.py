ec8b4af3b51f9cea4594201357841536d52f932f
Odilia Lirani <oclirani@gmail.com>
2020-06-14 13:18:59 -0500

    1409

---
+++ medium/1409.py
@@ -0,0 +1,14 @@
+class Solution:
+  def processQueries(self, queries: List[int], m: int) -> List[int]:
+    arr = []
+    p = [*range(1,m+1)]
+    
+    for i in range(0, len(queries)):
+      j = queries[i]
+      arr.append(p.index(j))
+
+      # want to move j to the front of p
+      p.remove(j)
+      p.insert(0,j)
+
+    return arr
